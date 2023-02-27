d31322200e0b81df9b605f695e61db58bf22e9f1
Murat Denizli <muratdenizli0@gmail.com>
2023-02-27 11:08:55 +0300

    link

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('https://lin-stock-app.herokuapp.com/'),
+    url: env('https://arcane-caverns-30725.herokuapp.com/'),
     app: { 
       keys: env.array('APP_KEYS')
     },
